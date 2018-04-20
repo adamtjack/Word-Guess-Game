bfc65175a357e762ffc8873ee647de8e9718719c
Adam Jack <adamjack@Adams-MacBook-Air.local>
2018-04-19 19:57:19 -0500

    complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,42 @@
 var selectableWords = [
-        "Nirvana",
-        "Smashing Pumkins",
-        "Weezer",
-        "Foo Fighters",
-        "Green Day",
-        "Red Hot Chili Peppers",
-        "Metallica",
-        "Rage Against the Machine",
-        "Blink-182",
-        "Backstreet Boys",
-        "The Offspring",
-        "Beastie Boys",
-        "Spice Girls",
-        "Goo Goo Dolls",
-        "Dave Matthews Band",
-        "Matchbox Twenty",
-        "Third Eye Blind",
-        "Stone Temple Pilots",
-        "Hootie and the Blowfish",
-        "Tupac Shakur",
-        "Dr Dre",
-        "Mariah Carey",
-        "Jay Z",
-        "Mary J Blige",
-        "Alanis Morissette",
-        "Missy Elliot",
-        "Sheryl Crow",
-        "Blues Traveler",
+        "nirvana",
+        "smashingpumpkins",
+        "weezer",
+        "foofighters",
+        "greenday",
+        "redhotchilipeppers",
+        "metallica",
+        "rageagainstthemachine",
+        "backstreetboys",
+        "theoffspring",
+        "beastieboys",
+        "spicegirls",
+        "googoodolls",
+        "davematthewsband",
+        "matchboxtwenty",
+        "thirdeyeblind",
+        "stonetemplepilots",
+        "hootieandtheblowfish",
+        "tupacshakur",
+        "drdre",
+        "mariahcarey",
+        "jay z",
+        "maryjblige",
+        "alanismorissette",
+        "missyelliot",
+        "sherylcrow",
+        "bluestraveler",
     ];
 
-const maxTries = 10;            // Maximum number of tries player has
+const maxTries = 8;            
 
-var guessedLetters = [];        // Stores the letters the user guessed
-var currentWordIndex;           // Index of the current word in the array
-var guessingWord = [];          // This will be the word we actually build to match the current word
-var remainingGuesses = 0;       // How many tries the player has left
-var gameStarted = false;        // Flag to tell if the game has started
-var hasFinished = false;        // Flag for 'press any key to try again'     
-var wins = 0;                   // How many wins has the player racked up
+var guessedLetters = [];        
+var currentWordIndex;           
+var guessingWord = [];          
+var remainingGuesses = 0;       
+var gameStarted = false;        
+var hasFinished = false;        
+var wins = 0;                   
 
 // Reset our game-level variables
 function resetGame() {
@@ -51,17 +50,14 @@ function resetGame() {
     guessedLetters = [];
     guessingWord = [];
 
-    // Make sure the hangman image is cleared
-    document.getElementById("hangmanImage").src = "";
-
     // Build the guessing word and clear it out
     for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
         guessingWord.push("_");
     }
     // Hide game over and win images/text
     document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
-    document.getElementById("gameover-image").style.cssText = "display: none";
-    document.getElementById("youwin-image").style.cssText = "display: none";
+    document.getElementById("youlose-image").style.cssText = "display: none";
+    document.getElementById("win-image").style.cssText = "display: none";
 
     // Show display
     updateDisplay();
@@ -78,8 +74,69 @@ function updateDisplay() {
     document.getElementById("remainingGuesses").innerText = remainingGuesses;
     document.getElementById("guessedLetters").innerText = guessedLetters;
     if(remainingGuesses <= 0) {
-        document.getElementById("gameover-image").style.cssText = "display: block";
+        document.getElementById("youlose-image").style.cssText = "display: block";
         document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
+        hasFinished = true;
+    }
+};
+
+document.onkeydown = function(event) {
+    // If we finished a game, dump one keystroke and reset.
+    if(hasFinished) {
+        resetGame();
+        hasFinished = false;
+    } else {
+        // Check to make sure a-z was pressed.
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key.toLowerCase());
+        }
+    }
+};
+function makeGuess(letter) {
+    if (remainingGuesses > 0) {
+        if (!gameStarted) {
+            gameStarted = true;
+        }
+
+        // Make sure we didn't use this letter yet
+        if (guessedLetters.indexOf(letter) === -1) {
+            guessedLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+    
+    updateDisplay();
+    checkWin();
+};
+// This function takes a letter and finds all instances of 
+// appearance in the string and replaces them in the guess word.
+function evaluateGuess(letter) {
+    // Array to store positions of letters in string
+    var positions = [];
+
+    // Loop through word finding all instances of guessed letter, store the indicies in an array.
+    for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
+        if(selectableWords[currentWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+
+    // if there are no indicies, remove a guess and update the hangman image
+    if (positions.length <= 0) {
+        remainingGuesses--;
+    } 
+    else {
+        // Loop through all the indicies and replace the '_' with a letter.
+        for(var i = 0; i < positions.length; i++) {
+            guessingWord[positions[i]] = letter;
+        }
+    }
+};
+function checkWin() {
+    if(guessingWord.indexOf("_") === -1) {
+        document.getElementById("win-image").style.cssText = "display: block";
+        document.getElementById("pressKeyTryAgain").style.cssText= "display: block";
+        wins++;
         hasFinished = true;
     }
 };
